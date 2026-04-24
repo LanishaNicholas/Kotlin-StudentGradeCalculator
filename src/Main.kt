1b590f583d11383c11de58dd32e2f814aa7401e0
Lanisha Nicholas <lanishanicholas@gmail.com>
2026-04-24 10:07:05 +0200

    NullPointerException and when operator

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-data class Student(val name: String, val scores: List<Int>)
+data class Student(val name: String, val scores: List<Int>, val email: String?)
 fun main() {
-    val s1 = Student("Lanisha",listOf(85,90,78))
-    val s2 = Student("Lanisha",listOf(85,90,78))
+    val s1 = Student("Adam",listOf(85,90,78), null)
+    val s2 = Student("Lanisha",listOf(85,90,78), "lanishanicholas@gmail.com")
 
     //toString
     println(s1)
@@ -18,7 +18,10 @@ fun main() {
     val (name, scores) = s1
     println("$name scored $scores")
 
+    println(s2)
+
     printReport(s1)
+    printReport(s2)
 
     //printReport(studentName,scores)
 }
@@ -26,9 +29,23 @@ fun main() {
 fun printReport(s1: Student) {
 
     with(s1){
+
         println("Name : $name")
+        println("Email : ${email ?: "No Email Provided"}")
         println("Total : ${scores.sum()}")
-        println("Average : ${scores.average()}")
+
+        val averageScore = scores.average()
+        println("Average : $averageScore")
+
+        val grade = when {
+            averageScore >= 90 -> "A"
+            averageScore >= 80 -> "B"
+            averageScore >= 70 -> "C"
+            else -> "F"
+        }
+
+        println("Grade: $grade")
+
         println("Passed : ${scores.filter { it >= 80 }}")
         println("Failed : ${scores.filter { it < 80 }}")
     }
